5412b8c2d8b90e5faba6f1c90697fd4de4d1b9d0
iza <117947026+izaizaiza@users.noreply.github.com>
2023-09-21 14:12:04 -0600

    re structured the html

--- app/week3/item.js
+++ app/week3/item.js
@@ -2,7 +2,7 @@
 
 export default function Item({ name, quantity, category }) {
     return (
-        <section className="flex flex-col justify-content: flex-start; p-24">
+        <section className="flex flex-col justify-content: flex-start; p-24 bg-">
             <h3 className="text-lg">{name}</h3>
             <p>Buy {quantity} in {category}</p>
         </section>
--- app/week3/page.js
+++ app/week3/page.js
@@ -4,7 +4,17 @@ import ItemList from "./item-list"
 export default function Page() {
     return (
         <main>
-            <ItemList></ItemList>
+            <nav>
+                <ul>
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/week2">Week2</Link></li>
+                </ul>
+            </nav>
+            <div>
+                <h1>Shopping List</h1>
+                <ItemList></ItemList>
+            </div>
+            
 
         </main>
     )
