55059b1d0794e970ddd4d5ad60f6ded01e651141
iza <117947026+izaizaiza@users.noreply.github.com>
2023-09-23 19:00:53 -0600

    edited Link st when github link is clicked it opens new tab

--- app/StudentInfo.js
+++ app/StudentInfo.js
@@ -2,11 +2,16 @@ import Link from 'next/link';
 
 export default function StudentInfo() {
     return (
-        <div>
-            <h1>Student Info</h1>
-            <p>Student Name: Iza Lumpio</p>
-            <p>Course Section: CPRG306-F</p>
-            <p>GitHub link: <Link href={"https://github.com/izaizaiza/cprg306-assignments.git"}>https://github.com/izaizaiza/cprg306-assignments.git</Link> </p>
+        <div class="bg-custom-slate flex-wrap: wrap; max-w-md rounded-md pt-2 pb-2">
+            <h1 class="text-xl ml-5 underline">Student Info</h1>
+            <p class= "text-md ml-5">Student Name: Iza Lumpio</p>
+            <p class= "text-md ml-5">Course Section: CPRG306-F</p>
+            <p class= "text-md ml-5">GitHub link: 
+                <Link 
+                    href={"https://github.com/izaizaiza/cprg306-assignments.git"}
+                    target="_blank">
+                        https://github.com/izaizaiza/cprg306-assignments.git
+                </Link> </p>
         </div>
     )
 }
