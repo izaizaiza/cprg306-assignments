fa141e40149a774c1505c41dc838673054a985a4
iza <117947026+izaizaiza@users.noreply.github.com>
2023-09-26 13:28:51 -0600

    fixed class to className issue

--- app/week2/page.js
+++ app/week2/page.js
@@ -7,23 +7,23 @@ export default function Week2() {
     return (
         <main>
 
-            <div class="flex flex-col">
+            <div className="flex flex-col">
 
-                <div class="flex flex-row">
-                    <h1 class="flex flex-grow ml-3 text-5xl">
+                <div className="flex flex-row">
+                    <h1 className="flex flex-grow ml-3 text-5xl">
                         CPRG 306: Web Development 2 - Assignments
                     </h1>
 
-                    <nav class="flex flex-shrink bg-custom-orange p-3">
+                    <nav className="flex flex-shrink bg-custom-orange p-3">
                         <Link href="/" class="hover:text-custom-slate">Home</Link>
                     </nav>
 
                 </div>
 
-                <div class="mt-10 mb-1 ml-10">
-                    <div class="flex flex-row">
-                        <hi class="flex-grow text-4xl ml-5 pt-20 mb-1 text-left align-middle">My Shopping List</hi>
-                        <StudentInfo class="justify-end flex-shrink"></StudentInfo>
+                <div className="mt-10 mb-1 ml-10">
+                    <div className="flex flex-row">
+                        <hi className="flex-grow text-4xl ml-5 pt-20 mb-1 text-left align-middle">My Shopping List</hi>
+                        <StudentInfo className="justify-end flex-shrink"></StudentInfo>
                     </div>
                 </div>
                 
