734217a4135ed09fea4e7da4cbbb94600bef42e0
iza <117947026+izaizaiza@users.noreply.github.com>
2023-09-26 13:28:10 -0600

    fixed class to className issue

--- app/StudentInfo.js
+++ app/StudentInfo.js
@@ -2,15 +2,15 @@ import Link from 'next/link';
 
 export default function StudentInfo() {
     return (
-        <div class="bg-custom-slate flex-wrap: wrap; max-w-md rounded-md pt-2 pb-2">
-            <h1 class="text-xl ml-5 underline">Student Info</h1>
-            <p class= "text-md ml-5">Student Name: Iza Lumpio</p>
-            <p class= "text-md ml-5">Course Section: CPRG306-F</p>
-            <p class= "text-md ml-5">GitHub link: 
+        <div className="bg-custom-slate flex-wrap: wrap; max-w-md rounded-md pt-2 pb-2">
+            <h1 className="text-xl ml-5 underline">Student Info</h1>
+            <p className= "text-md ml-5">Student Name: Iza Lumpio</p>
+            <p className= "text-md ml-5">Course Section: CPRG306-F</p>
+            <p className= "text-md ml-5">GitHub link: 
                 <Link 
                     href={"https://github.com/izaizaiza/cprg306-assignments.git"}
                     target="_blank"
-                    class="hover:text-custom-orange">
+                    className="hover:text-custom-orange">
                         https://github.com/izaizaiza/cprg306-assignments.git
                 </Link> </p>
         </div>
