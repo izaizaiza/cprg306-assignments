1705dce6a337bad2a3cafb196beaf0bf53c56ded
iza <117947026+izaizaiza@users.noreply.github.com>
2023-09-26 14:32:14 -0600

    added styling and navbar

--- app/week4/new-item.js
+++ app/week4/new-item.js
@@ -4,6 +4,7 @@
 
 import { useState } from "react"; 
 
+
 export default function NewItem(){
 
     const [name, setName] = useState("");
@@ -13,39 +14,86 @@ export default function NewItem(){
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        //console.log("form submitted");
-        console.log(name);
-        console.log(quantity);
-        console.log(category);
-
         
+        const item = {name, quantity, category };
+        console.log(item);
+
+        alert(`You are submitting name:${name}, quantity:${quantity}, and quantity:${category}.`);
+
+        // reset state of variables
+        setName("");
+        setQuantity(1);
+        setCategory("produce");
+
     }
 
     return(
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="name">Name</label>
-            <input 
-                id="name" 
-                type="text" 
-                value={name} 
-                onChange={(event) => setName(event.target.value)} 
-            />
-            <label htmlFor="quantity">Quantity</label>
-            <input 
-                id="quantity" 
-                type="text" 
-                value={quantity} 
-                onChange={(event) => setQuantity(event.target.value)} 
-            />
-            <label htmlFor="category">Category</label>
-            <input 
-                id="category" 
-                type="text" 
-                value={category} 
-                onChange={(event) => setCategory(event.target.value)} 
-            />
-            <button type="submit">Submit</button>
-        </form>
+        <main>
+            
+            <div className="m-10">
+                <h2 className="text-center">Add New Item</h2>
+                <form onSubmit={handleSubmit}
+                    className = "flex flex-row justify-center  rounded-md bg-custom-slate">
+                    
+                    <div className="flex flex-col m-5 space-y-5">
+                        <label htmlFor="name">Name</label>
+                        <label htmlFor="quantity">Quantity</label>
+                        <label htmlFor="category">Category</label>
+
+                    </div>
+
+                    <div className="flex flex-col m-5 space-y-5">
+                        <input
+                            id="name"
+                            type = "text"
+                            value = {name}
+                            onChange = {(event) => setName(event.target.value)}
+                            className = "bg-black rounded-md text-custom-white"
+                        />
+
+                        <input
+                            id = "quantity"
+                            type="number"
+                            min={1}
+                            max={99}
+                            value = {quantity}
+                            onChange = {(event) => setQuantity(event.target.value)}
+                            className = "bg-black rounded-md text-custom-white"
+                        />
+
+                        <select
+                        id = "category"
+                        value = {category}
+                        onChange={(event) => setCategory(event.target.value)}
+                        className = "bg-black rounded-md text-custom-white"
+                        >
+                            <option value="produce">Produce</option>
+                            <option value="dairy">Dairy</option>
+                            <option value="meat">Meat</option>
+                            <option value="bakery">Bakery</option>
+                            <option value="frozen foods">Frozen Foods</option>
+                            <option value="canned goods">Canned Goods</option>
+                            <option value="dry goods>">Dry Goods</option>
+                            <option value="beverages">Beverages</option>
+                            <option value="snacks">Snacks</option>
+                            <option value="household">Household</option>
+                            <option value="other">Other</option>
+                        </select>
+
+                        <submit 
+                        type="submit"
+                        className = "bg-custom-slate hover:bg-custom-orange w-20 text-center rounded-md"
+                        >
+                            Submit</submit>
+
+                    </div>
+    
+                </form>
+
+            </div>
+            
+
+        </main>
     )
 
 
--- app/week4/page.js
+++ app/week4/page.js
@@ -1,11 +1,23 @@
 
 import NewItem from "./new-item"
 import React from "react"
+import Link from 'next/link'
 
 export default function Page() {
 
     return(
         <main>
+
+            <div className="flex flex-row">
+                <h1 className="flex flex-grow ml-3 text-5xl">
+                    CPRG 306: Web Development 2 - Assignments
+                </h1>
+
+                <nav className="flex flex-shrink bg-custom-orange p-3">
+                    <Link href="/" class="hover:text-custom-slate">Home</Link>
+                </nav>
+
+            </div>
 
             <NewItem></NewItem>
 
