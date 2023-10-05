4b1afd9916d4c4417fbd7beedd63ad8404cc53cf
iza <117947026+izaizaiza@users.noreply.github.com>
2023-10-05 14:50:09 -0600

    re added h1

--- app/week3/page.js
+++ app/week3/page.js
@@ -1,11 +1,36 @@
 import ItemList from "./item-list"
 import Link from 'next/link'
+import StudentInfo from "../StudentInfo"
 
 
 export default function Page() {
     return (
-        <main>        
-            <ItemList class="flex-wrap: wrap; max-w-5xl"></ItemList>
+        <main>
+            <div className="flex flex-col">
+                <div className="flex flex-row">
+                    <h1 className="flex flex-grow ml-3 text-5xl">
+                        CPRG 306: Web Development 2 - Assignments
+                    </h1>
+
+                    <nav className="flex flex-shrink bg-custom-orange p-3 self-start">
+                        <Link href="/" class="hover:text-custom-slate">Home</Link>
+                    </nav>
+
+                </div>
+
+                <div className="mt-10 mb-1 ml-10">
+                    <div className="flex flex-row">
+                        <hi className="flex-grow text-4xl ml-5 pt-20 mb-1 text-left align-middle">My Shopping List</hi>
+                        <StudentInfo className="justify-end flex-shrink"></StudentInfo>
+                    </div>
+                </div>
+
+                <div>
+                    <ItemList class="flex-wrap: wrap; max-w-5xl"></ItemList>
+                </div>
+
+                </div>
+            
         </main>
     )
 }
