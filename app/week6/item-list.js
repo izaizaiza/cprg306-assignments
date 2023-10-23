a5f17de50dee19191cec3bbdfbb21ba223c5ca5e
iza <117947026+izaizaiza@users.noreply.github.com>
2023-10-22 23:54:17 -0600

    added week6 setup

---
+++ app/week6/item-list.js
@@ -0,0 +1,68 @@
+"use client"
+
+
+import { useState } from "react";
+import itemsData from "./items.json";
+import Item from "./item.js";
+
+
+
+export default function ItemList() {
+
+    
+    
+    // 1.initialize state variable
+    // create state variable named sortBy and setter function setSortBy
+    // set initial value to "name"
+    let [sortBy, setSortBy] = useState("name");
+
+    //2. Sort the items use javaScript's sort function to sort the items array
+    // if sortBy is name, sort the items by name
+    // if sortBy is category, sort the items by category
+    
+    let sortedItems = itemsData.sort((a, b) => {
+        if (sortBy === "name") {
+            return a.name.localeCompare(b.name);
+        } else {
+            return a.category.localeCompare(b.category);
+        }
+    });
+
+
+    console.log(sortedItems)
+    
+    
+
+    return (
+        <div>
+        <div>
+            <div
+            className="flex flex-row gap-5"
+            >
+                <p>Sort by:</p>
+
+                <button
+                className="p-1 w-40 bg-custom-orange hover:text-custom-slate "
+                onClick={() => setSortBy('name')}
+                >
+                    Sort by Name
+                </button>
+
+                <button
+                className="p-1 w-40 bg-custom-orange hover:text-custom-slate"
+                onClick={() => setSortBy('category')}
+                >
+                    Sort by Category
+                </button>
+            </div>
+            
+        </div>
+        <ul>
+            {sortedItems.map(item => (
+            <Item key={item.id} {...item} />
+            ))}
+        </ul>
+        </div>
+        
+    );
+}
---
+++ app/week6/item.js
@@ -0,0 +1,11 @@
+
+
+export default function Item({ name, quantity, category }) {
+    return (
+        <div className="p-4 my-4 ml-4 bg-custom-slate flex-wrap: wrap; max-w-md">
+            <h3 className="text-xl">{name}</h3>
+            <p className= "text-xs">Buy {quantity} in {category}</p>
+        </div>
+        
+    );
+  }
